bb8c8ae393134d13e42ad418d37bc505ae27fc51
Martin Barksten <martin.barksten@gmail.com>
2017-05-14 12:17:48 +0200

    Handle all incoming messages and send a heart beat
    All incoming messages are routed to the correct function and a heart beat is
    properly started when the player is registered.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## TODO:
 - [x] Set up basic project structure
 - [x] Set up basic communication with server
-- [ ] Implement full-suite of messages
+- [x] Implement full-suite of messages
 - [ ] Add utility functions
 - [ ] Add snakebot client class
 - [ ] Document how to install and run project (i.e. `pipenv shell` etc)
--- client/client.py
+++ client/client.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import sys
+import time
 
 import asyncio
 import colorlog
@@ -15,6 +16,18 @@ loop = asyncio.get_event_loop()
 class Connection(WebSocketClientProtocol):
     def __init__(self):
         super(WebSocketClientProtocol, self).__init__()
+        self.routing = {
+            messages.GAME_ENDED: self._game_ended,
+            messages.TOURNAMENT_ENDED: self._tournament_ended,
+            messages.MAP_UPDATE: self._map_update,
+            messages.SNAKE_DEAD: self._snake_dead,
+            messages.GAME_STARTING: self._game_starting,
+            messages.PLAYER_REGISTERED: self._player_registered,
+            messages.INVALID_PLAYER_NAME: self._invalid_player_name,
+            messages.HEART_BEAT_RESPONSE: self._heart_beat_response,
+            messages.GAME_LINK_EVENT: self._game_link,
+            messages.GAME_RESULT_EVENT: self._game_result
+        }
 
     def onOpen(self):
         log.info("connection is open")
@@ -37,6 +50,7 @@ class Connection(WebSocketClientProtocol):
         if reason:
             log.error(reason)
 
+        self.heart_beat.cancel()
         loop.stop()
 
     def _send(self, msg):
@@ -44,12 +58,51 @@ class Connection(WebSocketClientProtocol):
         self.sendMessage(json.dumps(msg).encode(), False)
 
     def _route_message(self, msg):
-        if msg['type'] == messages.PLAYER_REGISTERED:
-            self._send(messages.start_game())
-        elif msg['type'] == messages.MAP_UPDATE:
-            self._send(messages.register_move('DOWN', msg))
-        elif msg['type'] == messages.GAME_ENDED:
-            self.sendClose()
+        fun = self.routing.get(msg['type'], None)
+        if fun:
+            fun(msg)
+        else:
+            self._unrecognied_message(msg)
+
+    def _game_ended(self, msg):
+        self.sendClose()
+
+    def _tournament_ended(self, msg):
+        self.sendClose()
+
+    def _map_update(self, msg):
+        self._send(messages.register_move('DOWN', msg))
+
+    def _snake_dead(self, msg):
+        pass
+
+    def _game_starting(self, msg):
+        pass
+
+    def _player_registered(self, msg):
+        self._send(messages.start_game())
+        player_id = msg['receivingPlayerId']
+        self.heart_beat = loop.create_task(self._send_heart_beat(player_id))
+
+    def _invalid_player_name(self, msg):
+        pass
+
+    def _heart_beat_response(self, msg):
+        pass
+
+    def _game_link(self, msg):
+        log.info('Watch game at: %s', msg['url'])
+
+    def _game_result(self, msg):
+        log.info('Received game results: %s', msg)
+
+    def _unrecognied_message(self, msg):
+        log.error('Received unrecognized message: %s', msg)
+
+    async def _send_heart_beat(self, player_id):
+        while True:
+            self._send(messages.heart_beat(player_id))
+            await asyncio.sleep(2)
 
 
 def main():
@@ -61,9 +114,10 @@ def main():
 
     coro = loop.create_connection(factory, host, port)
     loop.run_until_complete(coro)
-    loop.run_forever()
-    loop.close()
 
+    loop.run_forever()
+
+    loop.close()
     sys.exit(0)
 
 
